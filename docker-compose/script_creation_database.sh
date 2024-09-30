98f5d44c25c5f7ab3bc98705bb22eaa9526dbcf2
Matthieu SERRANO <matthieu.serrano@outlook.com>
2024-09-30 11:45:42 +0200

    add init.sql

--- docker-compose/script_creation_database.sh
+++ docker-compose/script_creation_database.sh
@@ -8,8 +8,10 @@ DB_MYSQL=$(docker ps --filter "ancestor=mysql" --format "{{.Names}}")
 
 
 docker cp /home/sanou/DST-Airlines/docker-compose/DST_Airlines.sql $DB_MYSQL:/script.sql
+echo "Création des utilisateurs."
+docker exec -i $DB_MYSQL mysql -u root -ppassword DST_AIRLINES < ./init.sql
 echo "Création de la base de données."
-docker exec -i $DB_MYSQL mysql -u sanou -ppassword DST_AIRLINES < ./DST_Airlines.sql
+docker exec -i $DB_MYSQL mysql -u root -ppassword DST_AIRLINES < ./DST_Airlines.sql
 
 
 
