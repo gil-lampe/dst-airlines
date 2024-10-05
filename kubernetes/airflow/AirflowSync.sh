05a4e7a2c5092e0b1321270d2805cad43e7f154f
Bruno Oliveira <oliveira.brn203@gmail.com>
2024-10-05 14:58:41 +0200

    deploy.sh-maj

--- kubernetes/airflow/AirflowSync.sh
+++ kubernetes/airflow/AirflowSync.sh
@@ -38,6 +38,8 @@ helm install airflow apache-airflow/airflow -f override.yaml \
 # kubectl port-forward svc/airflow-webserver 8080:8080 --namespace airlines
 
 
+
+
 # helm install airflow apache-airflow/airflow \
 # --namespace airlines \
 # --set airflow.image.repository=glampe/dst_airlines_custom_airflow \
@@ -47,22 +49,3 @@ helm install airflow apache-airflow/airflow -f override.yaml \
 # --set postgresql.auth.postgresPassword="$pg_pw" \
 # --set postgresql.auth.username="$pg_user" \
 # --set postgresql.auth.password="$pg_pw"
-
-
-
-
-
-# helm upgrade --install airflow apache-airflow/airflow -f override.yaml \
-# --namespace airlines \
-# --set dags.persistence.enabled=false \
-# --set dags.gitSync.enabled=true \
-# --set dags.persistence.existingClaim=airflow-pvc-dags \
-# --set logs.persistence.enabled=true \
-# --set logs.persistence.existingClaim=airflow-pvc-logs \
-# --set airflow.extraAnnotations."prometheus.io/scrape"="true" \
-# --set airflow.extraAnnotations."prometheus.io/port"="8080"
-
-
-
-#  helm upgrade --install $RELEASE_NAME apache-airflow/airflow -f override.yaml --namespace $NAMESPACE --set dags.persistence.enabled=false --set dags.gitSync.enabled=true
-
--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,6 +1,9 @@
 kubectl create namespace airlines
 helm install mongodb ./mongo-DB/ -n airlines
 helm helm install mysql ./mysql/ -n airlines
+helm helm install fastapi ./fastapi/ -n airlines
+bash ./airflow/AirflowSync.sh
+
 
 # Post Forwarding du webserver
 #kubectl port-forward svc/airflow-webserver 8080:8080 --namespace airflow
