074c308ab889dae6b8fadf0b606c7f71239f4fdd
Bruno Oliveira <oliveira.brn203@gmail.com>
2024-10-06 14:43:50 +0200

    onfermeboutique

---
+++ kubernetes/onfermeboutique.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+helm uninstall mongodb -n airlines
+helm uninstall mysql -n airlines
+helm uninstall fastapi -n airlines
+helm uninstall airflow -n airlines
+
+kubectl delete -f ./airflow/pv-dags.yaml -n airlines
+kubectl delete -f ./airflow/pvc-dags.yaml -n airlines
+kubectl delete -f ./airflow/pv-logs.yaml -n airlines
+kubectl delete -f ./airflow/pvc-logs.yaml -n airlines
+
+kubectl delete namespace airlines
