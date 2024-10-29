7de559716e170d4b3cc3e797dc3e1d30d0a1392f
gil-lampe <lampe.gil@gmail.com>
2024-10-29 16:39:27 +0100

    Testing updated function

--- airflow/dags/dag_train_ml_model.py
+++ airflow/dags/dag_train_ml_model.py
@@ -20,7 +20,7 @@ sql_database = os.getenv("MYSQL_DATABASE")
 
 
 ###
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 import pandas as pd
 
 def get_tables(table_names: list[str], sql_user: str, sql_password: str, sql_host: str="localhost", sql_port: str="3306", sql_database: str="DST_AIRLINES") -> list[pd.DataFrame]:
@@ -46,6 +46,62 @@ def get_tables(table_names: list[str], sql_user: str, sql_password: str, sql_hos
 
     logger.info(f"Data download form the {table_names = } finalized.")
     return dataframes
+
+
+def upload_data_in_mysql(data: pd.DataFrame | pd.Series, table_name: str, sql_user: str, sql_password: str, if_exists: str="append", sql_host: str="localhost", sql_port: str="3306", sql_database: str="DST_AIRLINES") -> None:
+    """Upload provided data into the named table from the MySQL database whose detailed are provided, 
+    will either add only new rows of the data into the table if it exists or create the table and insert data into it if it does not already exist
+
+    Args:
+        data (pd.DataFrame): Data to be inserted into the MySQL table
+        table (str): Name of the MySQL table
+        sql_user (str): Username to be used to connect to the MySQL database
+        sql_password (str): Password
+        if_exists (str, optional): Method to use if the table already exists, see `DataFrame.to_sql()` for more details. Defaults to "append".
+        sql_host (str, optional): MySQL host to use to connect. Defaults to "localhost".
+        sql_port (str, optional): MySQL port to use to connect. Defaults to "3306".
+        sql_database (str, optional): MySQL database name to which to connect. Defaults to "DST_AIRLINES".
+    """
+    logger.info(f"Initiating data upload into the the {table_name = }.")
+
+    # Création de la connexion avec la base de données MySQL
+    connection_string = f"mysql+pymysql://{sql_user}:{sql_password}@{sql_host}:{sql_port}/{sql_database}"
+    engine = create_engine(connection_string)
+
+    # Récupération du nom des tables, s'il y en a
+    inspector = inspect(engine)
+    table_names = inspector.get_table_names()
+
+    # Si la table existe, ajout des nouvelles lignes uniquement, sinon création de la table et ajout des données
+    if table_name in table_names:
+        logger.info(f"{table_name = } is found in the {sql_database = }, appending new rows only into the table.")
+
+        # Conversion en DataFrame si les données sont de type Series
+        new_data = pd.DataFrame(new_data) if isinstance(new_data, pd.Series) else new_data
+        
+        # Récupération des données existantes
+        existing_data = pd.read_sql(f"SELECT * FROM {table_name}", con=engine)
+        
+        # Sélection des nouvelles données à ajouter uniquement
+        new_data = data.merge(existing_data, on=list(data.columns), how='left', indicator=True)
+        new_data = new_data[new_data['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+        new_data_row_number = new_data.shape[0]
+
+        # Insertion des données à la base MySQL
+        number_rows_appended = new_data.to_sql(name=table_name, con=engine, if_exists=if_exists, index=False)
+
+        logger.info(f"New rows inserted in the {table_name = }, ({number_rows_appended = } vs. {new_data_row_number = }).")
+        
+    else:
+        logger.info(f"{table_name = } not found in the {sql_database = }, creating the table and inserting data into it.")
+
+        # Création de la table et insertion des données
+        number_rows_appended = data.to_sql(name=table_name, con=engine, if_exists=if_exists, index=False)
+        data_row_number = data.shape[0]
+
+        logger.info(f"{table_name = } created and row inserted, ({number_rows_appended = } vs. {data_row_number = }).")
+
 
 ###
 
@@ -85,8 +141,8 @@ def taskflow():
         mysql.upload_data_in_mysql(data=features, table_name="features", sql_user=sql_user, sql_password=sql_password, sql_host=sql_host, sql_port=sql_port, sql_database=sql_database)
 
         target = prepare_data.compute_target_delay_in_minutes(flights_weather_forecasts)
-        mysql.upload_data_in_mysql(data=target, table_name="target", sql_user=sql_user, sql_password=sql_password, sql_host=sql_host, sql_port=sql_port, sql_database=sql_database)
-
+        # mysql.upload_data_in_mysql(data=target, table_name="target", sql_user=sql_user, sql_password=sql_password, sql_host=sql_host, sql_port=sql_port, sql_database=sql_database)
+        upload_data_in_mysql(data=target, table_name="target", sql_user=sql_user, sql_password=sql_password, sql_host=sql_host, sql_port=sql_port, sql_database=sql_database)
 
     @task()
     def compute_model_score(model_name: str, prev_task: None=None) -> float:
