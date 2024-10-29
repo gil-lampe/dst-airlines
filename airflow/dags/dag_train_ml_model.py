edaa97686daeb16fb88e43912f4a62ab0c627fd4
gil-lampe <lampe.gil@gmail.com>
2024-10-29 16:51:51 +0100

    Testing updated function

--- airflow/dags/dag_train_ml_model.py
+++ airflow/dags/dag_train_ml_model.py
@@ -77,7 +77,9 @@ def upload_data_in_mysql(data: pd.DataFrame | pd.Series, table_name: str, sql_us
         logger.info(f"{table_name = } is found in the {sql_database = }, appending new rows only into the table.")
 
         # Conversion en DataFrame si les données sont de type Series
-        data = data.to_frame() if isinstance(data, pd.Series) else data
+        if isinstance(data, pd.Series):
+            data = data.to_frame('0')
+            data.columns.astype(str)
         logger.info(f"{list(data.columns) = }")
         
         # Récupération des données existantes
